5ab00cc96fe012bf00cf84e8b8b18bd4487444e2
Johannes Habel <EchterAlsFake@proton.me>
2026-07-10 20:25:45 +0200

    - big updates

--- tube8_api/__init__.py
+++ tube8_api/__init__.py
@@ -1,3 +1,3 @@
 __all__ = ["Client", "Video", "Channel", "Pornstar"]
 
-from tube8_api.redtube_api import Video, Client, Channel, Pornstar
+from tube8_api.tube8 import Video, Client, Channel, Pornstar
